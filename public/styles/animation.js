cd4b430907f0d0b5348d056191f995f35cb1b4b4
Cesar Gomez <zootechdrum@gmail.com>
2018-10-28 19:07:39 -0700

    added Jquery animation to skills

---
+++ public/styles/animation.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.skillbar').each(function(){
+		$(this).find(".skillbar-bar").animate({
+			width:$(this).attr("data-percent")
+
+		},6000)
+		console.log("Hellow")
+	});
+});
