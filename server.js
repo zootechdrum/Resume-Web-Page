cd4900a8fb52dd7d9eb5552eb9dab8897011a3af
Cesar Gomez <zootechdrum@gmail.com>
2019-12-13 13:23:41 -0800

    deleted old code. time for a refresh

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 
 const express = require('express');
 const hbs = require('hbs');
-const fs = require('fs');
-
 const port = process.env.PORT || 3000;
 
 
@@ -10,31 +8,8 @@ const app = express();
 hbs.registerPartials(__dirname + '/views/partials')
 
 
-app.use((req, res, next) => {
-    const now = new Date().toString();
-    const log = `${now}: ${req.method} ${req.url}`;
-    console.log(log);
-    fs.appendFile('server.log', log + '/n', (err) => {
-        if (err) {
-            console.log('Unable to append to server.log.')
-        }
-    });
-    next();
-});
-
-// app.use((req, res, next) => {
-//     res.render('maintenance.hbs')
-// });
-
 app.use(express.static(__dirname + '/public'));
 
-hbs.registerHelper("getCurrentYear", () => {
-    return new Date().getFullYear();
-});
-
-hbs.registerHelper("screamIt",(text) => {
-    return text.toUpperCase();
-})
 
 app.set('view engine','hbs');
 
@@ -45,16 +20,5 @@ app.get('/', (req,res) => {
     });
 });
 
-app.get('/about',(req,res) => {
-    res.render('about.hbs', {
-        pageTitle:"About Page",
-    });
-});
 
-app.get('/bad', (req,res) => {
-    res.send({
-        errorMessage:'Unable to handle request'
-    });
-});
-
-app.listen(port)
+app.listen(port )
